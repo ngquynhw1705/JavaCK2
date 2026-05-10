4fb00589458bf488a3bf7517f949298052d2e691
ThaoVy2404 <ntthaovy2404@gmail.com>
2026-05-10 15:49:41 +0700

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String URL = "jdbc:mysql://localhost:3306/clinic-db?useUnicode=true&characterEncoding=UTF-8";
+	private static final String URL = "jdbc:mysql://localhost:3306/clinic_db?useUnicode=true&characterEncoding=UTF-8";
 	private static final String USER = "root";
 	private static final String PASSWORD = "12345678";
 
